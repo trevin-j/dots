48477e0ab20018b280ec8851c8f3e05ab90019c3
Trevin Jones <tr3vinj@gmail.com>
2025-12-16 03:44:54 -0700

    add emoji picker

--- tl/meta/manifest.sh
+++ tl/meta/manifest.sh
@@ -1,3 +1,7 @@
 export requires=()
 export require_aur="false"
 export deps=(fzf kitty cliphist wl-clipboard wtype rbw)
+post_stow() {
+    data_file="$HOME/.local/share/tl/emoji.json"
+    curl -sL "https://github.com/milesj/emojibase/raw/refs/heads/master/packages/data/en/compact.raw.json" -o "$data_file" || return 1
+}
