e4e9cd416dae38bfb4b6c60da4643291c18b6406
Trevin Jones <tr3vinj@gmail.com>
2025-12-16 03:55:46 -0700

    add nerd font picker

--- tl/meta/manifest.sh
+++ tl/meta/manifest.sh
@@ -2,6 +2,7 @@ export requires=()
 export require_aur="false"
 export deps=(fzf kitty cliphist wl-clipboard wtype rbw)
 post_stow() {
-    data_file="$HOME/.local/share/tl/emoji.json"
-    curl -sL "https://github.com/milesj/emojibase/raw/refs/heads/master/packages/data/en/compact.raw.json" -o "$data_file" || return 1
+    data_dir="$HOME/.local/share/tl"
+    curl -sL "https://github.com/milesj/emojibase/raw/refs/heads/master/packages/data/en/compact.raw.json" -o "$data_dir/emoji.json" || return 1
+    curl -sL "https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/glyphnames.json" -o "$data_dir/nerd-font-icons.json" || return 1
 }
