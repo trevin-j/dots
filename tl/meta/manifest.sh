e095432e90903d258d5a19288e20c82eaaf64fab
Trevin Jones <tr3vinj@gmail.com>
2025-12-18 19:45:27 -0700

    fix broken manifest hood

--- tl/meta/manifest.sh
+++ tl/meta/manifest.sh
@@ -3,6 +3,7 @@ export require_aur="false"
 export deps=(fzf kitty cliphist wl-clipboard wtype rbw)
 post_stow() {
     data_dir="$HOME/.local/share/tl"
+    mkdir -p "$data_dir"
     curl -sL "https://github.com/milesj/emojibase/raw/refs/heads/master/packages/data/en/compact.raw.json" -o "$data_dir/emoji.json" || return 1
     curl -sL "https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/glyphnames.json" -o "$data_dir/nerd-font-icons.json" || return 1
 }
