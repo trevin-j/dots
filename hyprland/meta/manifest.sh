29048a348715b94eb11664362de033ff19bc372e
Trevin Jones <tr3vinj@gmail.com>
2026-01-09 23:28:18 -0700

    switch to dolphin file explorer and autoinstall fix for dolphin applications

--- hyprland/meta/manifest.sh
+++ hyprland/meta/manifest.sh
@@ -1,7 +1,9 @@
 export requires=(foot theming tl waybar)
 export require_aur="true"
 export deps=(
-    thunar gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler libgsf tumbler-extra-thumbnailers webp-pixbuf-loader
+    sudo bash coreutils
+    archlinux-xdg-menu
+    dolphin kservice ark audiocd-kio baloo dolphin-plugins kio-admin kompare konsole ffmpegthumbs icoutils kdegraphics-thumbnailers kdesdk-thumbnailers kimageformats libappimage qt6-imageformats resvg taglib
     firefox
     brightnessctl
     wl-clipboard
@@ -11,3 +13,12 @@ export deps=(
     hyprland
     kdeconnect
 )
+
+pre_dl() {
+    # Here we want to automatically update dolphin's available programs on every system upgrade/install/removal.
+    sudo pacman -S --needed archlinux-xdg-menu sudo bash coreutils kservice
+    # The above are required deps in order to process apps.
+    # Copy the hook to where it should go
+    sudo mkdir -p /etc/pacman.d/hooks/
+    sudo cp "$DOTDIR/hyprland/meta/updateKDEcache.hook" /etc/pacman.d/hooks/updateKDEcache.hook
+}
