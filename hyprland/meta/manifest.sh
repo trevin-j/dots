c2f2595a5e5b96f4bf8c882075cf64c486170485
Trevin Jones <tr3vinj@gmail.com>
2026-03-22 20:18:32 -0600

    fix hyprland deps

--- hyprland/meta/manifest.sh
+++ hyprland/meta/manifest.sh
@@ -1,8 +1,6 @@
 export requires=(foot theming waybar zellij)
 export pacman_deps=(
-    sudo bash coreutils
-    archlinux-xdg-menu
-    dolphin kservice ark audiocd-kio baloo dolphin-plugins kio-admin kompare konsole ffmpegthumbs icoutils kdegraphics-thumbnailers kdesdk-thumbnailers kimageformats libappimage qt6-imageformats resvg taglib
+    dolphin ark audiocd-kio baloo dolphin-plugins kio-admin kompare konsole ffmpegthumbs icoutils kdegraphics-thumbnailers kdesdk-thumbnailers kimageformats libappimage qt6-imageformats resvg taglib
     firefox
     brightnessctl
     wl-clipboard
@@ -11,9 +9,9 @@ export pacman_deps=(
     hyprland
     kdeconnect
     hyprsunset
-    bibata-cursor-theme-bin
     grim slurp
     quickshell
+    ttf-material-symbols-variable
 )
 export aur_deps=(
     bibata-cursor-theme-bin
