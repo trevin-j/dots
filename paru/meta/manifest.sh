a3c162f45837cfcc9805dc027210c1b756a41bbf
Trevin Jones <devtrev00@gmail.com>
2026-04-10 22:04:08 -0600

    add paru config

---
+++ paru/meta/manifest.sh
@@ -0,0 +1,13 @@
+# Required packages to have already been installed from this repo
+export requires=()
+
+# System dependencies installed via pacman
+export pacman_deps=()
+
+# System dependencies installed via an AUR helper
+export aur_deps=()
+
+# Optional hook scripts (must be executable):
+#   meta/pre_dl      runs before dependency installation
+#   meta/pre_stow    runs before stowing
+#   meta/post_stow   runs after stowing
